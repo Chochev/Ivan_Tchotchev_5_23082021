c8eebe5d63ea4dafdfd03caa95d36294396f116c
Ivan Tchotchev <ivan_dirst@yahoo.com>
2021-08-30 11:28:33 +0200

    price convert func added

---
+++ frontend/scriptJS/functions.js
@@ -0,0 +1,11 @@
+ //price convert
+ priceConvert = ittemPrice => {
+    let price = `${ittemPrice}`;
+    price = Intl.NumberFormat ("fr-FR", {
+        style:"currency",
+        currency: "EUR",
+        fractionDigits:2,
+    }).format(price/100);
+    return price;
+    
+}
