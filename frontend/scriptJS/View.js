1fae39641cb67f4e6b9ece5af82a818040bed0f8
Ivan Tchotchev <ivan_dirst@yahoo.com>
2021-09-02 14:31:41 +0200

    style changing

--- frontend/scriptJS/View.js
+++ frontend/scriptJS/View.js
@@ -1,13 +1,22 @@
 class View {
     showListOurson(listOurson) {
         for(ittem of listOurson){
-                        const cards = document.getElementById("mainContainer")
+                         const cards = document.getElementById("mainContainer")
                         // cards =  document.createElement("cards");
+                
                         const price = priceConvert(ittem.price);
-                        cards.innerHTML += ` <a href="index.html?_id=${ittem._id}"><img src="${ittem.imageUrl}" class="mainContainer" alt="${ittem.name}"></a>
+                        cards.innerHTML += `<div class="card"> <div class="cardImg"><a href="index.html?_id=${ittem._id}"><img src="${ittem.imageUrl}"  alt="${ittem.name}"></a></div>
+                        <div class="cardDescription">
+                        <div class="namePrice">
                         <h3>${ittem.name} </h3>
                         <h3>${price} </h3>
+                        </div>
+                        <div class="ittemInfo">
                         <p>${ittem.description} </p>
+                        </div>
+                        </div>
+                        </div>
+                        
                         `
                     }
     }
